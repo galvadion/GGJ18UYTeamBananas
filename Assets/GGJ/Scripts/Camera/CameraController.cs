e60825fa5e5b81ec5e9c9ca82cc5528f5c1e743a
galvadion <urashimasan@gmail.com>
2018-01-27 11:55:24 -0300

    2 controles
    Se agrega movimiento a dos jugadores

---
+++ Assets/GGJ/Scripts/Camera/CameraController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TeamUtility.IO;
+
+public class CameraController
+	: MonoBehaviour
+{
+    void Update()
+	{
+		
+	}
+}

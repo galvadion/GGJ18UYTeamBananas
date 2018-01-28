7754e21e463d9c98d4920b5aed50351a6a58ae5c
Fito Casella <rodocasella@gmail.com>
2018-01-28 10:35:01 -0300

    Animated lava, changes on ground texture and lighting

--- Assets/GGJ/Scripts/Entities/FloorEntity.cs
+++ Assets/GGJ/Scripts/Entities/FloorEntity.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FloorEntity : MonoBehaviour {
-
-
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+public class FloorEntity : MonoBehaviour
+{
+	private void Start()
+	{
+		int i = Random.Range(0, 4);
+		if (i == 1)
+		{
+			transform.Rotate(Vector3.up, 90);
+		}
+		else if (i == 2)
+		{
+			transform.Rotate(Vector3.up, 180);
+		}
+		else if (i == 3)
+		{
+			transform.Rotate(Vector3.up, -90);
+		}
 	}
 }
--- Assets/LavaAnimator.cs
+++ Assets/LavaAnimator.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class LavaAnimator : MonoBehaviour
 {
-	Material m;
+	public Color colorA = Color.white;
+	public Color colorB = Color.white;
+
+	private Color lerpedColor;
+	private Material m;
+
 	private void Start()
 	{
 		m = GetComponent<MeshRenderer>().material;
+	}
+	void Update()
+	{
+		lerpedColor = Color.Lerp(colorA, colorB, Mathf.PingPong(Time.time, 1));
+		m.SetColor("_EmissionColor", lerpedColor);
+
 	}
 }

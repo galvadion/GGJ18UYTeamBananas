cf0b0fbd0422afc0f3669c97b6f36cdbafda12b5
galvadion <urashimasan@gmail.com>
2018-01-28 01:08:03 -0300

    Merge
    merge

---
+++ Assets/GGJ/Scripts/Managers/FloorBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorBehaviour : MonoBehaviour {
+	List<GameObject> floors;
+	float timer;
+	bool active;
+	GameObject floorEntity;
+	// Use this for initialization
+	void Start () {
+	//	 floors =   GameObject.FindGameObjectsWithTag ("Floor");
+		int floorLength =  GameObject.FindGameObjectsWithTag ("Floor").Length;
+		//active = true;
+		InvokeRepeating ("trembleFloor", 5, 15);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		/*if (active) {
+
+			this.GetComponent<Animator> ().SetTrigger ("destroy");
+			Destroy (floorEntity);
+			active = false;
+		}*/
+	}
+
+
+	void trembleFloor()
+	{
+		active = true;
+			timer = 0f;
+			floorEntity = GameObject.FindGameObjectsWithTag ("Floor") [Random.Range (0, 340)];
+		floorEntity.GetComponent<Animator> ().SetTrigger ("destroy");
+		Destroy (floorEntity);
+		active = false;
+
+	}
+
+
+}
